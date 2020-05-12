9e47bd550a103d27f8631d5fea45a535101ca39b
Maksim Romanov <ios38ru@gmail.com>
2020-05-12 22:18:18 +0800

    Set up difficulty selection

--- Millionaire/MainMenuController.m
+++ Millionaire/MainMenuController.m
@@ -9,10 +9,19 @@
 #import "MainMenuController.h"
 #import "Game.h"
 
+typedef enum {
+    easy,medium,hard,insane
+} Difficulty;
+
 @interface MainMenuController ()
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *difficultySegmentedControl;
+//@property (assign,nonatomic) Difficulty selectedDifficulty;
 
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 @property (weak, nonatomic) IBOutlet UIButton *resultsButton;
+
+- (IBAction)testButton:(id)sender;
 
 @end
 
@@ -23,6 +32,34 @@
     
     [self.startButton addTarget:self action:@selector(startGameSession) forControlEvents:UIControlEventTouchUpInside];
     [self.resultsButton addTarget:self action:@selector(showResults) forControlEvents:UIControlEventTouchUpInside];
+}
+/*
+- (Difficulty) selectedDifficulty {
+    switch (self.difficultySegmentedControl.selectedSegmentIndex) {
+        case 0:
+            NSLog(@"Easy difficulty selected");
+            return easy;
+            break;
+        case 1:
+            NSLog(@"Medium difficulty selected");
+            return medium;
+            break;
+        case 2:
+            NSLog(@"Hard difficulty selected");
+            return hard;
+            break;
+        case 3:
+            NSLog(@"Insane difficulty selected");
+            return insane;
+            break;
+        default:
+            return medium;
+            break;
+    }
+}*/
+
+- (IBAction)testButton:(id)sender {
+    NSLog(@"Selected difficulty: %ld", (long)self.difficultySegmentedControl.selectedSegmentIndex);
 }
 
 - (void) startGameSession {
